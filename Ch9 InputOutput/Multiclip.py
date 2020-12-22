f203c144e649657bad1d3a59d3d9e3edd3beb315
Daman Gill <1gilldam2@gmail.com>
2020-12-21 21:59:34 -0500

    Finish multiclip

--- Ch9 InputOutput/Multiclip.py
+++ Ch9 InputOutput/Multiclip.py
@@ -1,14 +1,19 @@
 #! python3
 import shelve, pyperclip, sys
+mclipShelf = shelve.open('mclip')
 
-mclipShelf = shelve.open("mclip")
-if len(sys.argv) == 3 and sys.argv[1] == 'save':
-    mclipShelf[sys.argv[2]] = pyperclip.paste()
+if len(sys.argv) == 3:
+    if sys.argv[1].lower() == 'save':
+        mclipShelf[sys.argv[2]] = pyperclip.paste()
+    elif sys.argv[1].lower() == 'delete':
+        del mclipShelf[sys.argv[2]]
 
 elif len(sys.argv) == 2:
     if sys.argv[1].lower() == 'list':
         pyperclip.copy(str(list(mclipShelf.keys())))
+    elif sys.argv[1].lower() == 'delete':
+        mclipShelf.clear()
     elif sys.argv[1] in mclipShelf:
         pyperclip.copy(mclipShelf[sys.argv[1]])
-
+    
 mclipShelf.close()
