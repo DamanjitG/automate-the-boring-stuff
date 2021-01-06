ce5262336010ce3c466c898e256932d0f28b31be
Daman Gill <1gilldam2@gmail.com>
2021-01-05 22:45:41 -0500

    Chapter 13 Spreadsheet Cell Inverter

---
+++ Ch13 Working with Excel Spreadsheets/SpreadsheetCellInverter.py
@@ -0,0 +1,15 @@
+import os, sys, openpyxl
+
+if len(sys.argv) == 2:
+    target = sys.argv[1]
+    inputWb = openpyxl.load_workbook(target)
+    inputSheet = inputWb.active
+    outputWb = openpyxl.Workbook()
+    outputSheet = outputWb.active
+    for rowNum in range(1, len(list(inputSheet.rows)) + 2):
+        for colNum in range(1, len(list(inputSheet.columns)) + 2):
+            outputSheet.cell(row = rowNum, column = colNum).value = inputSheet.cell(row = colNum, column = rowNum).value
+
+    outputWb.save(target)
+
+else: print("Please pass only one input; the path to the excel spreadsheet you wish to invert.")
